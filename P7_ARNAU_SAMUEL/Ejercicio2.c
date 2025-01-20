5373bf74d064144f8217653f271d814c7ca7a43e
SamuelACR <samuelcarias1@gmail.com>
2025-01-20 10:03:50 +0100

    Ejercicio2 definido el struct

--- P7_ARNAU_SAMUEL/Ejercicio2.c
+++ P7_ARNAU_SAMUEL/Ejercicio2.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAX_TAMANIO 50;
+#define NOTA_MAXIMA 11;
+typedef enum{
+	nombre[MAX_TAMANIO];
+	apellido[MAX_TAMANIO];
+	nota[NOTA_MAXIMA]
+}estudiante;
 int main(){
 
 
