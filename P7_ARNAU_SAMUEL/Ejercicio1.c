065cf9627baff6ac31acaf3e5e7d22401e685b2e
SamuelACR <samuelcarias1@gmail.com>
2025-01-20 10:43:28 +0100

    Intento de Ejercicio1 Final

--- P7_ARNAU_SAMUEL/Ejercicio1.c
+++ P7_ARNAU_SAMUEL/Ejercicio1.c
@@ -8,10 +8,20 @@
 	x = (int **) realloc(*x,n * sizeof(int));
 }
 */
+int encontrar_menor(int const * numero1, int n){
+	int menor = numero1[0];
+	for (int i = 1; i < n; i++){//
+		if (menor > numero1[i]){
+			menor = numero1[i];
+		}
+	}
+	return menor;
+}
 int main(){
 	int exit;
 	int vueltas = 0;
 	int * numeros;
+	int * ordenados;
 	printf("Escribe cuantos numeros quieras y escribe exit para salir\n");
 	numeros = (int *) malloc(sizeof(int));
 	do{
@@ -20,7 +30,10 @@ int main(){
 		numeros = (int *) realloc(numeros, vueltas * sizeof(int));
 		exit = scanf(" %d", &numeros[vueltas-1]);
 	}while(exit != 0);
-	
+	ordenados = (int *) malloc(vueltas * sizeof(int));
+	for(int i = 0; i < vueltas; i++){
+	ordenados[i] = encontrar_menor(numeros, vueltas);
+	}
 	free(numeros);
 return 0;
 }
