fa49e7aab21059f82e273fcc4f4e5a46e1326389
SamuelACR <samuelcarias1@gmail.com>
2025-01-20 09:37:50 +0100

    Ejercicio 1

--- P7_ARNAU_SAMUEL/Ejercicio1.c
+++ P7_ARNAU_SAMUEL/Ejercicio1.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+	Trabajo realizado por Arnau y Samuel
+*/
 int main(){
-
+	int exit;
+	printf("Escribe cuantos numeros quieras y escribe exit para salir\n");
+	do{
+		ajustar_tamanio();
+		scanf("%d");
+	}while(exit == 0);
 
 return 0;
 }
