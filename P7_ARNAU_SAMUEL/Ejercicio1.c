c2f64cf5fcd2e6acc7734a19bade31a733bc2617
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-01-20 10:03:19 +0100

    malloc y realloc (sale mal)

--- P7_ARNAU_SAMUEL/Ejercicio1.c
+++ P7_ARNAU_SAMUEL/Ejercicio1.c
@@ -3,14 +3,23 @@
 /*
 	Trabajo realizado por Arnau y Samuel
 */
+
+void ajustar_tamanio(int ** x, int n){
+	x = (int **) realloc(*x,n * sizeof(int));
+}
+
 int main(){
 	int exit;
+	int vueltas = 0;
+	int * numeros;
 	printf("Escribe cuantos numeros quieras y escribe exit para salir\n");
+	numeros = (int *) malloc(sizeof(int));
 	do{
-		ajustar_tamanio();
-		scanf("%d");
-	}while(exit == 0);
-
+		vueltas++;
+		ajustar_tamanio(&numeros, vueltas);
+		exit = scanf(" %d", numeros);
+	}while(exit != 0);
+	free(numeros);
 
 return 0;
 }
