e4ddf8ee937c7f18caa3b6c96c15650f93ac59d2
SamuelCR-lab <samuelcarias1@gmail.com>
2025-01-21 14:25:28 +0100

    Funcion creada de intercambiar valores

--- P7_ARNAU_SAMUEL/Ejercicio1.c
+++ P7_ARNAU_SAMUEL/Ejercicio1.c
@@ -8,6 +8,12 @@ void ajustar_tamanio(int ** puntero_a_puntero, int tamanio){
 	*puntero_a_puntero = (int *) realloc(*puntero_a_puntero,tamanio * sizeof(int));
 }
 
+void intercambiarValores(int * primero, int * numero_m){
+	int posicion = *primero;
+	*primero = *numero_m;
+	*numero_m = posicion;
+}
+
 int encontrar_menor(int const * numero1, int n){
 	int menor = numero1[0];
 	for (int i = 1; i < n; i++){//
@@ -22,7 +28,6 @@ int main(){
 	int exit;
 	int vueltas = 0;
 	int * numeros;
-	int * ordenados;
 	printf("Escribe cuantos numeros quieras y escribe exit para salir\n");
 	numeros = (int *) malloc(sizeof(int));
 	do{
@@ -31,9 +36,9 @@ int main(){
 		//numeros = (int *) realloc(numeros, vueltas * sizeof(int));
 		exit = scanf(" %d", &numeros[vueltas-1]);
 	}while(exit != 0);
-	ordenados = (int *) malloc(vueltas * sizeof(int));
+
 	for(int i = 0; i < vueltas; i++){
-	ordenados[i] = encontrar_menor(numeros, vueltas);
+	encontrar_menor(numeros, vueltas);
 	}
 	free(numeros);
 return 0;
