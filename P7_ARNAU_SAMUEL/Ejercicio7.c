d3d02724d3d22db9851e943c98109ae05f25f167
SamuelCR-lab <samuelcarias1@gmail.com>
2025-01-28 13:38:33 +0100

    pequeño avance dia de la excursion

--- P7_ARNAU_SAMUEL/Ejercicio7.c
+++ P7_ARNAU_SAMUEL/Ejercicio7.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+void array(int m);
+void primos_encontrados(int m);
 
+int main(){
+	int m, primos, primos_encontrados;
+
+	printf("Introduce un número cualquiera: ");
+	scanf(" %d",&m);
+	primos = primos_encontrados(m);
+	int * arr = array(primos);
+	guardado_primos(primos); 
 
 return 0;
+}
+void array(int m){
+	arr = (int*) malloc(m * sizeof(int));
+}
+void primos_encontrados(int m){
+	for(int i = 1; i = m ; i++){
+		primos = i/i;
+		if (primos = 1){
+			primos_encontrados = primos + primos_encontrados;
+		}
+	}
+}
+void guardado_primos(int primos){
+	for(int i = 1; i = m ; i++){
+		primos = i/i;
 }
