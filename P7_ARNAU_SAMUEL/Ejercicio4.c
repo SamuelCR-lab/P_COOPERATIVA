5f89b23bf2ae2175cfea075a0cac182e3ee257a0
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-02-06 21:18:07 +0100

    Hago el ejercicio 4 básico

--- P7_ARNAU_SAMUEL/Ejercicio4.c
+++ P7_ARNAU_SAMUEL/Ejercicio4.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int comprobar_palindromo(char * string, int longitud, int i);
 
 int main(){
+	char palindromo[100];
+	printf("Escribe una palablra/frase: ");
+	scanf(" ");
+	fgets(palindromo, 100, stdin);
+	if(comprobar_palindromo(palindromo, strlen(palindromo)-1, 1)){
+		printf("Se puede leer del derecho y del revés!\n");
+	} else {
+		printf("No se puede leer del derecho y del revés\n");
+	}
+	return 0;
+}
 
+int comprobar_palindromo(char * string, int longitud, int i){
+	char ini = string[0];
+	char fin = string[longitud-i];
+	if(longitud/2 <= 2){
+		if(ini == fin){
+			return 1;
+		} else {
+			return 0;
+		}	
+	}
 
-return 0;
+	if(ini == fin){
+		return comprobar_palindromo(&string[1], --longitud, ++i);
+	} else {
+		return 0;
+	}
+	return 0;
 }
