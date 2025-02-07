2f094c2feb06e24997f402f8972aed5b51c09d36
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-02-07 15:37:36 +0100

    Añado comentarios

--- P7_ARNAU_SAMUEL/Ejercicio3.c
+++ P7_ARNAU_SAMUEL/Ejercicio3.c
@@ -4,70 +4,69 @@
 /*
 	Trabajo realizado por Arnau y Samuel
 */
-char ** crear_tablero(int n,int m);
-void borde_tablero (char ** borde, int n, int m);
-void imprimir_tablero(char ** inicio, int n,int m);
-void movimiento(char ** movimiento,int * n,int * m, char sbid);
+char ** crear_tablero(int n,int m); // Para reservar el espacio del tablero (sobre todo si lo elije el usuario)
+void borde_tablero (char ** borde, int n, int m); // Para rellenar el tablero de espacios con un borde de asteriscos
+void imprimir_tablero(char ** inicio, int n,int m); // Para limpiar la pantall y mostrar el tablero
+void movimiento(char ** movimiento,int * n,int * m, char sbid); // Para moverse
 
 int main(int argcount, char ** argvalue){
 	int n,m;//n son filas y m columnas
-	int * x;
-	int * y;
-	int tmpx, tmpy;
+	int x; // Posición filas
+	int y; // Posición columnas
 	char ** tablero; 
-	char sbid;
+	char sbid; // subir, bajar, izquierda y derecha
 	if(argcount == 1){
 		n = 12;
 		m = 12;
-		tmpx = n/2;
-		x = &tmpx;
-		tmpy = m/2;
-		y = &tmpy;
+		x = n/2; // Lo dividimos entre 2 para que empiece en el medio
+		y = m/2;
 		tablero = crear_tablero(n,m);
-		borde_tablero(tablero,n,m);
+		borde_tablero(tablero,n,m); // Mostramos el tablero con el # en medio
 		do{
 			scanf(" %c",&sbid);
-			movimiento(tablero,x,y,sbid);
+			movimiento(tablero,&x,&y,sbid); // Le pasamos la posición de # por referencia para que se guarde cuando acabe la función
 			imprimir_tablero(tablero,n,m);
-		} while (sbid == 'w' || sbid == 's' || sbid == 'd' || sbid == 'a');
+		} while (sbid == 'w' || sbid == 's' || sbid == 'd' || sbid == 'a'); // Hasta que no escriba otra cosa, no acaba el bucle
 
 	}else if(argcount == 3){
-		if (argvalue[1] < 0){
-			printf("Introduce un número positivo para el ancho y poder crear un tablero");
-		}else if (argvalue[2] < 0){
-			printf("Introduce un número positivo para el largo y poder crear un tablero");
+		if (atoi(argvalue[1]) < 2){
+			printf("Introduce un número más grande para el ancho y poder crear un tablero\n");
+			return 1; // Para que se acabe el programa
+		}else if (atoi(argvalue[2]) < 2){
+			printf("Introduce un número más grande para el largo y poder crear un tablero\n");
+			return 1;
 		}else{
+
 			n = atoi(argvalue[1]);
 			m = atoi(argvalue[2]);
-			tmpx = n/2;
-			x = &tmpx;
-			tmpy = m/2;
-			y = &tmpy;
+			x = n/2;
+			y = m/2;
 			tablero = crear_tablero(n,m);
 			borde_tablero(tablero,n,m);
 			do{
 				scanf(" %c",&sbid);
-				movimiento(tablero,x,y,sbid);
+				movimiento(tablero,&x,&y,sbid);
 				imprimir_tablero(tablero,n,m);
 			} while (sbid == 'w' || sbid == 's' || sbid == 'd' || sbid == 'a');
-			
 		}
 
 	}else{
 		printf("Error pocos o demasiados argumentos por lineas de comandos, puedes escribir el ancho y largo del tablero\n");
-        return 0;
+        return 1;
 	}
+	
 	for(int i = 0; i < n; i++){
 			free(tablero[i]);
-		}
-		free(tablero);
-return 0;
+	}
+	free(tablero);
+
+	return 0;
 }
 char ** crear_tablero(int n,int m){
-	char ** arr= (char**) malloc(n * sizeof(char*));
+	char ** arr= (char**) malloc(n * sizeof(char*)); // Reserva de memoria para las filas
 		if(arr == NULL) return 0;
 		for(int i = 0; i < n; i++){
-			arr[i] = (char*) malloc(m * sizeof(char));
+			arr[i] = (char*) malloc(m * sizeof(char)); // Reserva de memoria para las columnas
 			if(arr[i] == NULL) return 0;
 		}
 		return arr;
@@ -76,9 +75,9 @@ void borde_tablero(char ** borde, int n, int m){
 	for (int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
 			if(i == 0 || i == n-1 || j == 0 || j == m-1){
-				borde[i][j] = '*';
+				borde[i][j] = '*'; // Ponemos * sólo en los bordes
 			}else{
-				borde[i][j] = ' ';
+				borde[i][j] = ' '; // El resto lo llenamos de esapacios
 			}
 		}
 	}
@@ -86,35 +85,35 @@ void borde_tablero(char ** borde, int n, int m){
 	imprimir_tablero(borde,n,m);
 }
 void imprimir_tablero(char ** inicio, int n,int m){
-	system("clear");
+	system("clear"); // Para limpiar la pantalla
 	for (int i = 0; i < n; i++){
-		printf("\t\t\t\t\t\t");
+		printf("\t\t\t\t\t\t"); // Tabulamos para que se vea más centrado
 		for(int j = 0; j < m; j++){
 			printf("%c", inicio[i][j]);
 		}
-		printf("\n");
+		printf("\n"); // Para pasar a la siguiente línea
 	}
 }
 void movimiento(char ** movimiento,int * n,int * m, char sbid){
 	switch (sbid){
 		case 'w':
 			movimiento[*n][*m] = ' ';
-			(*n)--;
+			(*n)--; // Para situarse en la línea de arriba
 			movimiento[*n][*m] = '#';
 			break;
 		case 's':
 			movimiento[*n][*m] = ' ';
-			(*n)++;
+			(*n)++; // Para situarse en la línea de abajo
 			movimiento[*n][*m] = '#';
 			break;
 		case 'd':
 			movimiento[*n][*m] = ' ';
-			(*m)++;
+			(*m)++; // Para situarse en la columna de la derecha
 			movimiento[*n][*m] = '#';
 			break;
 		case 'a':
 			movimiento[*n][*m] = ' ';
-			(*m)--;
+			(*m)--; // Para situarse en la columna de la izquierda
 			movimiento[*n][*m] = '#';
 			break;
 	}
