94c1c97bf6afbbfc61e99be7365e88629ef0b4f5
SamuelCR-lab <samuelcarias1@gmail.com>
2025-02-17 13:28:18 +0100

    Renombramiento de texto.h a defines.h

--- Juego_P/JuegoPrincipal.h
+++ Juego_P/JuegoPrincipal.h
@@ -4,3 +4,5 @@
 int menu();
 void mejorar_stats(Caballero * stats, int * monedas);
 void batalla(Caballero * jugador, Dragon * npc);
+void ataqueCaballero(Caballero * jugador, Dragon * npc);
+void ataqueDragon(Caballero * jugador, Dragon * npc);
