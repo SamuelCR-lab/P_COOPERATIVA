47e9ae378c02c315c6082ec0718099a0b9ee4d57
D3LucasX <d3lucasx@gmail.com>
2025-02-20 14:04:24 +0100

    inicio a la tienda del inframundo

--- Juego_P/batallas.c
+++ Juego_P/batallas.c
@@ -3,9 +3,11 @@
 #include <unistd.h>
 #include <time.h>
 #include "JuegoPrincipal.h"
+#include <string.h>
 
 void batalla(Caballero * jugador, Dragon npc){ // Pasamos dragón por valor para que cuando acabe la batalla, el dragón tenga las mismas estadísticas que antes.
     int ronda = 1;
+    static int vidas = 1;
     srand(time(NULL)); // crea una llista de numeros random en funcion de los segundos del PC
                        // Cada vez que hace rand() selecciona el siguiente número de la lista.
 	while (jugador->vidaActual >= 0 && npc.vidaActual >= 0){
@@ -17,14 +19,31 @@ void batalla(Caballero * jugador, Dragon npc){ // Pasamos dragón por valor para
                 printf("El caballero %s ha ganado la batalla\n", jugador->nombre);
                 jugador->monedas += npc.monedas;
                 jugador->vidaActual += jugador->vida * 0.3;
-                break;
+                if (strcmp(npc.vida,"Charizard") == 0){
+                    return 3;
+                }else{
+                    return 0;
+                }
             }
             ataqueDragon(jugador, &npc);
             if (jugador->vidaActual <= 0){
                 jugador->vidaActual = 0;
                 printf("El dragon %s ha ganado la batalla\n", npc.nombre);
-                system("cat < Muerte.txt");
-                break;
+                if(vidas == 1){
+                    vidas--;
+                    system("cat < Dios.txt");
+                    printf("lore");
+                    jugador->monedas /= 2;
+                    return 0;
+                }else if(vidas == 0){
+                    vidas--;
+                    system("cat < Muerte.txt");
+                    printf("lore");
+                    return 1;
+                }else{
+                    printf("Ya no hay mas intentos, GAME OVER");
+                    return 2;
+                }
             }
         }else{
             ataqueDragon(jugador, &npc);
@@ -49,3 +68,7 @@ void batalla(Caballero * jugador, Dragon npc){ // Pasamos dragón por valor para
         jugador->vidaActual = jugador->vida;
     }
 }
+
+// TODO: si te mata un dragon, viene dios y te revive con la mitad de la vida.
+//       Si te vuelve a matar, viene el demonio y te lleva a la tienda del inframundo y te vende almas, que cada una tiene una salud
+//       basadas en las monedas que tengs. Si no tienes monedas, carricoche.
